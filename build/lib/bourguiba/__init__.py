3bf2e3adf0d29911cd3a6cc022fc32d0f98887f2
AzizBahloul <azizbahloul3@gmail.com>
2024-09-28 12:50:07 +0100

    gpt 2

--- build/lib/bourguiba/__init__.py
+++ build/lib/bourguiba/__init__.py
@@ -1,3 +1,5 @@
-from .bourguiba import Bourguiba
-
-__all__ = ['Bourguiba']
+# This file can be left empty or include package-level docstring
+"""
+Bourguiba: A package for generating shell commands based on descriptions.
+"""
+from .bourguiba import main  # Import main function for command line execution
--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='Bourguiba',  # Replace with your package name
-    version='1.0.9',   # Increment this with each release
-    packages=find_packages(),
-    install_requires=[
-        'torch', 
-        'transformers'
-    ],
-    entry_points={
-        'console_scripts': [
-            'bourguiba=bourguiba.bourguiba:main',  # Point to your main function
-        ],
-    },
-    author='si mohamed aziz bahloul wo chrikou il mangouli mahdi magroun',  # Replace with your name
-    author_email='azizbahloul3@gmail.com',  # Replace with your email
-    description='A command line generator based on user input',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/AzizBahloul/PromptToCommand',  # Replace with your repo link
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Adjust if necessary
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',  
-)
+setup()
