97781692bc532fd827f9b7db56cd55922b4b1a32
AzizBahloul <azizbahloul3@gmail.com>
2025-02-02 15:42:20 +0100

    use deep seek r1

--- bourguibaGPT/main.py
+++ bourguibaGPT/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import platform
 import re
 import sys
--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bourguibaGPT',
-    version='0.1.0',
-    author='Siaziz',
-    author_email='azizbahloul3@gmail.com',
-    description='A prompt to command program that generates terminal commands based on user input.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/AzizBahloul/PromptToCommand.git',
+    name="prompt to command",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        'transformers',
-        'torch',
-        'textblob',
-        'pyspellchecker',
-        'numpy<2.0',
+        "requests",
+        "rich",
     ],
+    entry_points={
+        "console_scripts": [
+            "shell-command-generator=shell_command_generator.main:main",
+        ],
+    },
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="Enhanced Shell Command Generator",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/shell_command_generator",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
